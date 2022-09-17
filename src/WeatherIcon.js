ee4bc789f8866e0a104f448c058803679228f553
amber moss <amoss@mossmedia.net>
2022-09-16 19:46:46 -0500

    display the forecast for one day

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#3282b8"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
