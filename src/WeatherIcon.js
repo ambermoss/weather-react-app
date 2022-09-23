bd91451953ecf117707b6563a5f1bc3bae7c68ec
amber moss <amoss@mossmedia.net>
2022-09-23 13:06:18 -0500

    changed icon color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#4588f7"
+      color="#215bf0"
       size={props.size}
       animate={true}
     />
